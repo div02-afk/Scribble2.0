20cfa815e6e6df09506ed3f4dc2360961f3d7cd0
Divyam Sharma <divyam.7379@gmail.com>
2023-12-30 11:16:59 +0530

    url changes

--- client/src/components/socket.jsx
+++ client/src/components/socket.jsx
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-const socket = io.connect("https://scribble2-0-server.vercel.app");
+const socket = io.connect("https://scribble2-0-server-bf7ri33tt-divyam-s-projects.vercel.app");
 console.log("socket", socket.connected);
 export default socket;
