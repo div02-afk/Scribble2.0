eb8dd08e20265d46713fa455dd0416ff1f163745
Divyam Sharma <divyam.7379@gmail.com>
2023-12-30 11:08:15 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://scribble2-0.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
